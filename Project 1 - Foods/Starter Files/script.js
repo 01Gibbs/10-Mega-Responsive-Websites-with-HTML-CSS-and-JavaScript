6806cafe47767b794eaf67f3937bab1b5edb5b87
01Gibbs <danielgilbert49@hotmail.co.uk>
2023-01-04 14:15:03 +0000

    Progress, Project 1

--- Project 1 - Foods/Starter Files/script.js
+++ Project 1 - Foods/Starter Files/script.js
@@ -1,17 +1,22 @@
-const icons = document.querySelectorAll(".section-1-icons i");
+// document.querySelector('.menu').addEventListener('click', () => {
+//   document.querySelectorAll('.target').forEach((item) => {
+//     item.classList.toggle('change');
+//   });
+// });
+
+const icons = document.querySelectorAll('.section-1-icons i');
 let i = 1;
 
 setInterval(() => {
   i++;
 
-  const icon = document.querySelector("section-1-icons.change");
+  const icon = document.querySelector('.section-1-icons .change');
+  icon.classList.remove('change');
 
-  icon.classList.remove("change");
-
-  if (i > i.length) {
-    icons[0].classlist.add("change");
+  if (i > icons.length) {
+    icons[0].classList.add('change');
     i = 1;
   } else {
-    icon.nextElementSibling.classList("change");
+    icon.nextElementSibling.classList.add('change');
   }
 }, 4000);
